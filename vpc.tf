23f19a667132c25104a26963bd3418f090919768
Heitor Morgado <heitor.morgado@skipthedishes.ca>
2025-08-05 09:57:59 -0600

    * main - secondary ipv6 and ipv4

--- vpc.tf
+++ vpc.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_version = ">= 1.9.2"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
 resource "aws_vpc" "main" {
   cidr_block                       = var.vpc_cidr
   enable_dns_support               = true
@@ -22,6 +7,16 @@ resource "aws_vpc" "main" {
   tags = {
     Name = "Production_Explore-vpc"
   }
+}
+
+resource "aws_vpc_ipv4_cidr_block_association" "secondary_ipv4" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.2.0.0/16"
+}
+
+resource "aws_vpc_ipv6_cidr_block_association" "secondary_ipv6" {
+  vpc_id          = aws_vpc.main.id
+  assign_generated_ipv6_cidr_block = true
 }
 
 resource "aws_internet_gateway" "igw" {
@@ -54,9 +49,8 @@ resource "aws_subnet" "private" {
   vpc_id                          = aws_vpc.main.id
   cidr_block                      = each.value
   availability_zone               = element(var.availability_zones, each.key)
-  # map_public_ip_on_launch         = true
   assign_ipv6_address_on_creation = true
-  ipv6_cidr_block = local.private_ipv6_cidrs[each.key]
+  ipv6_cidr_block                 = local.private_ipv6_cidrs[each.key]
   tags = {
     Name = "Production_Explore-subnet-private${each.key + 1}-${element(var.availability_zones, each.key)}"
   }
@@ -104,5 +98,24 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
-# TODO add VPC Endpoint
-# TODO add ipv6 to route tables
+# IPv4 and IPv6 subnets - chap 3 "Create Subnets"
+resource "aws_subnet" "isolated_dev_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.64.0/26"
+  availability_zone = var.availability_zones[0]
+  tags = {
+    Name = "Isolated_Dev_US_West_1a"
+  }
+}
+
+# us-west-1b wasn't available
+resource "aws_subnet" "isolated_dev_c" {
+  vpc_id                          = aws_vpc.main.id
+  cidr_block                      = "10.0.65.0/26" # due to error: MissingParameter: Either 'cidrBlock' or 'ipv4IpamPoolId' should be provided. https://github.com/hashicorp/terraform-provider-aws/issues/42414
+  availability_zone               = var.availability_zones[1]
+  assign_ipv6_address_on_creation = true
+  ipv6_cidr_block                 = "2600:1f1c:fbb:1e06::/64"
+  tags = {
+    Name = "Isolated_Dev_US_West_1c"
+  }
+}
