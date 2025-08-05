a811905c775745486956d1cfa05c6b208756a35e
Heitor Morgado <heitor.morgado@skipthedishes.ca>
2025-08-04 20:52:35 -0600

    - first commit

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.0.0/20", "10.0.16.0/20"]
+}
+
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.128.0/20", "10.0.144.0/20", "10.0.160.0/20", "10.0.176.0/20"]
+}
+
+variable "availability_zones" {
+  type    = list(string)
+  default = ["us-west-1a", "us-west-1c"]
+}
---
+++ vpc.tf
@@ -0,0 +1,99 @@
+terraform {
+  required_version = ">= 1.9.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "Production_Explore-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "Production_Explore-igw"
+  }
+}
+
+resource "aws_subnet" "public" {
+  for_each = { for idx, cidr in var.public_subnet_cidrs : idx => cidr }
+
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = each.value
+  availability_zone       = element(var.availability_zones, each.key)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Production_Explore-subnet-public${each.key + 1}-${element(var.availability_zones, each.key)}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  for_each = { for idx, cidr in var.private_subnet_cidrs : idx => cidr }
+
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = each.value
+  availability_zone       = element(var.availability_zones, each.key)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Production_Explore-subnet-private${each.key + 1}-${element(var.availability_zones, each.key)}"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "Production_Explore-rtb-public"
+  }
+}
+
+resource "aws_route_table" "private" {
+  for_each = { for idx, cidr in var.private_subnet_cidrs : idx => cidr }
+
+
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Production_Explore-rtb-private${each.key + 1}-${element(var.availability_zones, each.key)}"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  for_each = { for idx, cidr in var.private_subnet_cidrs : idx => cidr }
+
+  subnet_id      = aws_subnet.private[each.key].id
+  route_table_id = aws_route_table.private[each.key].id
+}
+
+resource "aws_route_table_association" "public" {
+  for_each = { for idx, cidr in var.public_subnet_cidrs : idx => cidr }
+
+  subnet_id      = aws_subnet.public[each.key].id
+  route_table_id = aws_route_table.public.id
+}
+
+# TODO add VPC Endpoint
