78da3efd942926d51025370bb089470687f1b2a1
Heitor Morgado <heitor.morgado@skipthedishes.ca>
2025-08-06 15:22:08 -0600

    * main - vpc peering

--- vpc.tf
+++ vpc.tf
@@ -20,6 +20,17 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_vpc_peering_connection" "dev_prod" {
+  peer_vpc_id = aws_vpc.main.id # target
+  vpc_id      = aws_vpc.dev.id  # requester
+
+  tags = {
+    "Name" = "Peer_Dev_Production_VPCs"
+  }
+
+  depends_on = [aws_vpc.main, aws_vpc.dev]
+}
+
 resource "aws_vpc_ipv4_cidr_block_association" "secondary_ipv4" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.2.0.0/16"
