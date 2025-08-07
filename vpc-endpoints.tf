38b2124bf5ea90c38c8f8094ae47e1a38b461d88
Heitor Morgado <heitor.morgado@skipthedishes.ca>
2025-08-07 14:29:30 -0600

    * main - more vpc endpoint

--- vpc-endpoints.tf
+++ vpc-endpoints.tf
@@ -9,3 +9,17 @@ resource "aws_vpc_endpoint" "s3" {
 
   tags = { "Name" : "S3 Gateway VPC Endpoint" }
 }
+
+resource "aws_vpc_endpoint" "interface" {
+  vpc_id            = aws_vpc.main.id
+  vpc_endpoint_type = "Interface" # default
+  service_name      = "com.amazonaws.us-west-1.rds"
+
+  #                    returns 2 subnets, one in each AZ
+  subnet_ids         = [for az in var.availability_zones : local.private_subnet_ids_map_per_region[az][0]]
+  security_group_ids = [aws_security_group.sg["database"].id]
+
+  tags = { "Name" : "RDS Interface VPC Endpoint" }
+
+  depends_on = [aws_security_group.sg]
+}
