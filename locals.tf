c1699849a0fb18a57733c0b1ce1db2aea1fe8eda
Heitor Morgado <heitor.morgado@skipthedishes.ca>
2025-08-20 17:09:08 -0600

    * main - better comment docs

--- locals.tf
+++ locals.tf
@@ -33,12 +33,14 @@ locals {
   # returns a list of ids for private route tables
   private_route_table_ids = [for idx in range(length(aws_route_table.private)) : aws_route_table.private[idx].id]
 
-  # the ellipsis (...) at the end of the expression allows for grouping (map) when the key (AZs) repeats, returning something like:
+  # the ellipsis (...) at the end of the expression allows for grouping (map) when the key (AZs) repeats, returning:
   # {
-  #   "us-west-1a" = ["subnet-0ec46e4e3i7926b72", "subnet-01987455113b06718"]
-  #   "us-west-1c" = ["subnet-01989784dae8e72d7", "subnet-096aa0cd873ea0388"]
+  #   "us-west-1a" = ["subnet-1", "subnet-2"]
+  #   "us-west-1c" = ["subnet-3", "subnet-4"]
   # }
-  private_subnet_ids_map_per_region = { for idx in range(length(aws_subnet.private)) : aws_subnet.private[idx].availability_zone => aws_subnet.private[idx].id ... }
+  private_subnet_ids_map_per_region = {
+    for idx in range(length(aws_subnet.private)) : aws_subnet.private[idx].availability_zone => aws_subnet.private[idx].id ...
+  }
 
   # needs "." at the end of FQDN to avoid perpetual changes
   blocked_websites = ["virus-765431fd89e.com.", "sdhfiuwwh98723475.info.", "defnotagoodwebsitetoclick.io."]
